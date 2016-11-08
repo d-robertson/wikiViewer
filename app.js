9134b2a19bdfdff5013e83e4ec219c4d92a80218
derek_robertson <dwilliamrobertson@gmail.com>
2016-11-08 10:43:11 -0800

    added full functionality and styling

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 angular.module('wikiApp', [])
 .controller('wikiCtrl', ['$scope', '$http', function($scope, $http){
-  $scope.search = 'dog';
+  $scope.search = '';
+  $scope.success = 'btnDiv';
   $scope.wikiSearch = function(){
 
     var req = {
@@ -17,10 +18,28 @@ angular.module('wikiApp', [])
     }
 
     $http(req).then(function success(res){
+      $scope.res = [];
       console.log('success: ', res);
+      for(var i = 0; i < res.data[1].length; i++){
+        var resObj = {
+          title: res.data[1][i],
+          description: res.data[2][i],
+          link: res.data[3][i]
+        }
+        $scope.res.push(resObj);
+        $scope.success = 'btnDivSearch';
+      }
+      console.log($scope.res);
     }, function error(res){
       console.log("error: ", res);
     });
   }
+  $scope.clearAll = function(){
+    if($scope.search === ''){
+      $scope.success = 'btnDiv';
+      $scope.res = [];
+    }
+  }
+  
   
 }]);
